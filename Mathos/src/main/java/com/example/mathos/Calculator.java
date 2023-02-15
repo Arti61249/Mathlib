83c23b7f7c05f8711cc1ff0a0960867c2fce6355
shravani <shravani.mula@thundersoft.com>
2023-02-15 16:41:19 +0530

    first commit

---
+++ Mathos/src/main/java/com/example/mathos/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.mathos;
+
+import android.content.Context;
+
+public class Calculator {
+    public static int add(Context context,int a, int b)
+    {
+        return a+b;
+    }
+}
